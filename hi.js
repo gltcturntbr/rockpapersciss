f549168a2f444af3956c9db8c4f6b124818b293f
coder123 <hisup197@gmail.com>
2024-05-19 22:34:34 -0400

    Add basic game functionality

--- hi.js
+++ hi.js
@@ -5,6 +5,34 @@
 
 */
 
+function choiceToNum(c) {
+  switch (c) {
+    case "rock":
+      return 0;
+    case "scissors":
+      return 1;
+    case "paper":
+      return 2;
+  }
+}
+
 while (true) {
-    console.log(Math.random());
+  let cChoice = Math.floor(Math.random() * 3);
+  let uChoice = choiceToNum(prompt("enter your choice :D (case sensitive)"));
+
+  if (cChoice == uChoice) {
+    alert("tie :(");
+  } else if (cChoice == 1 && uChoice == 0) {
+    alert("you win :D");
+  } else if (cChoice == 2 && uChoice == 0) {
+    alert("you lose :(");
+  } else if (cChoice == 0 && uChoice == 1) {
+    alert("you lose :(");
+  } else if (cChoice == 2 && uChoice == 1) {
+    alert("you win :D");
+  } else if (cChoice == 0 && uChoice == 2) {
+    alert("you win :D");
+  } else if (cChoice == 1 && uChoice == 2) {
+    alert("you lose :(");
+  }
 }
